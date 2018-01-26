85f4dba14d3f3310b294d43af7327ec5aae0b6e3
Scott M Stark <starksm64@gmail.com>
2018-01-25 21:53:23 -0800

    Log the health check

--- src/main/java/org/wildfly/swarm/osbapi/tokenservice/HealthEndpoint.java
+++ src/main/java/org/wildfly/swarm/osbapi/tokenservice/HealthEndpoint.java
@@ -5,6 +5,7 @@ import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.jboss.logging.Logger;
 
 /**
  * The health check for the JWTService
@@ -12,8 +13,11 @@ import org.eclipse.microprofile.health.HealthCheckResponse;
 @Health
 @ApplicationScoped
 public class HealthEndpoint implements HealthCheck {
+    private static final Logger log = Logger.getLogger(HealthEndpoint.class);
+
     @Override
     public HealthCheckResponse call() {
+        log.debug("jwt-service-broker-check up");
         return HealthCheckResponse.named("jwt-service-broker-check")
                 .up()
                 .build();
